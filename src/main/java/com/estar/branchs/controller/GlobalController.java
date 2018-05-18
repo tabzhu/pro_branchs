55576d941a7422e9462a54be4743e199e08eec1c
tabzhu <tabzhu112@sina.com>
2018-05-18 09:42:17 +0800

    指定增强器的范围

--- src/main/java/com/estar/branchs/controller/GlobalController.java
+++ src/main/java/com/estar/branchs/controller/GlobalController.java
@@ -1,9 +1,15 @@
 package com.estar.branchs.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+/**
+ * 指定只针对RestController注解、Controller注解标注的控制器类进行增强
+ *
+ */
+@RestControllerAdvice(annotations = {RestController.class, Controller.class})
 public class GlobalController {
 
 	@ExceptionHandler(value = {Exception.class})
